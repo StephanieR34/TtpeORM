c241d7021b499e49986379cde15cb0f54db0dcfb
StephanieR34 <34.regnier.stephanie@gmail.com>
2022-01-31 17:19:24 +0100

    module typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,26 @@ import { AppService } from './app.service';
 import { FirstMiddleware } from './middlewares/first.middleware';
 import { TodoModule } from './todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
 @Module({
   imports: [
     TodoModule,
     ConfigModule.forRoot({
       isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
     })
   ],
   controllers: [AppController],
